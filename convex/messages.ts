703b87cc0a9d5659a7987752cd7679084ea565b4
Ashwani Singh <ashwani.singh@westwing.de>
2024-09-01 17:23:11 +0200

    feat: add like to messages

--- convex/messages.ts
+++ convex/messages.ts
@@ -6,15 +6,42 @@ export const list = query({
   handler: async (ctx) => {
     // Grab the most recent messages.
     const messages = await ctx.db.query("messages").order("desc").take(100);
+    const messagesWithLikes = await Promise.all(
+      messages.map(async (message) => {
+        const likes = await ctx.db
+        .query("likes")
+        .withIndex("byMessageId", (q) => q.eq("messageId", message._id))
+        .collect();
+        return {
+          ...message,
+          likes: likes.length,
+          body: message.body.replaceAll(":)", "🙂"),
+        };
+      }),
+    );
+
     // Reverse the list so that it's in a chronological order.
-    return messages.reverse();
+    return messagesWithLikes.reverse();
   },
 });
 
 export const send = mutation({
   args: { body: v.string(), author: v.string() },
-  handler: async (ctx, { body, author }) => {
+  handler: async (ctx, args) => {
     // Send a new message.
-    await ctx.db.insert("messages", { body, author });
+    await ctx.db.insert("messages", {
+      body: args.body,
+      author: args.author,
+    });
+  },
+});
+
+export const like = mutation({
+  args: { messageId: v.id("messages"), liker: v.string() },
+  handler: async (ctx, args) => {
+    await ctx.db.insert("likes", {
+      messageId: args.messageId,
+      liker: args.liker,
+    });
   },
 });
